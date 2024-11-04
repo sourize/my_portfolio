53b1b5756d5996e031ed8d2540c371d4be066844
sourize <sourishchatterjee0067@gmail.com>
2024-11-04 13:25:04 +0530

    File Updated

--- app/page.tsx
+++ app/page.tsx
@@ -17,50 +17,28 @@ export default function Page() {
       </a>
 
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
-        Portfolio, made simple!
+        Sourish Chatterjee
       </h1>
 
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
-          Vercel, and Tailwind CSS for optimal performance.
+          I'm an aspiring Data Scientist and ML Engineer, passionate about transforming data into impactful real-world solutions.
         </p>
         <p>
-          Nextfolio includes all the essentials for a stunning portfolio: SEO,
-          MDX support, RSS, Atom, & JSON feeds, analytics, tweet & YouTube
-          embeds, KaTeX integration, and{" "}
+          I'm currently studying B.Tech in Computer Science Engineering, focusing on AI and ML at {" "}
           <a
             target="_blank"
-            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
+            href="https://msit.edu.in"
           >
-            more
+            MSIT
           </a>
           .
         </p>
         <p>
-          Nextfolio is{" "}
-          <a href={socialLinks.github} target="_blank">
-            open-source
-          </a>{" "}
-          and fully customizable, making it easy to add more features.
+        Beyond tech, I enjoy sharing insights on data science and personal projects through blog posts, participating in hackathons, and staying connected with the industry.
         </p>
         <p>
-          <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
-            target="_blank"
-          >
-            Deploy
-          </a>{" "}
-          your Nextfolio site with Vercel in minutes and follow the set up
-          instructions in the{" "}
-          <a href="/blog/getting-started">Getting Started</a> post.
-        </p>
-        <p>
-          Built and maintained by{" "}
-          <a href="https://imsirius.xyz/" target="_blank">
-            Sirius
-          </a>
-          .
+        Let's connect! I'm open to collaborations and discussions on shared interests.
         </p>
       </div>
     </section>
